02d1a973a764a5d742b4e33d6d460ba11c782717
guilhermewt <welliston10@gmail.com>
2022-07-29 22:14:55 -0300

    Sobrescrevendo handler do Spring

--- src/main/java/com/projetoTrabalhador/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetoTrabalhador/resources/exceptions/ResourceExceptionHandler.java
@@ -4,19 +4,25 @@ import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.WebUtils;
 
 import com.projetoTrabalhador.service.exceptions.BadRequestException;
 import com.projetoTrabalhador.service.exceptions.BadRequestExceptionDetails;
+import com.projetoTrabalhador.service.exceptions.ExceptionDetails;
 import com.projetoTrabalhador.service.exceptions.ValidationExceptionDetails;
 
 @ControllerAdvice 
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(BadRequestException.class)
 	public ResponseEntity<BadRequestExceptionDetails> HandlerBadRequestException(BadRequestException bre){
@@ -31,9 +37,9 @@ public class ResourceExceptionHandler {
 				, HttpStatus.BAD_REQUEST);
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<ValidationExceptionDetails> HandlerMethodArgumentNotValidException(MethodArgumentNotValidException exception){
-		
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {	
 		List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
 		String field = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(","));
 		String fieldMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(","));
@@ -48,6 +54,21 @@ public class ResourceExceptionHandler {
 				.fieldMessage(fieldMessage)
 				.build()
 				, HttpStatus.BAD_REQUEST);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(
+			Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+		.timestamp(Instant.now())
+		.status(status.value())
+		.title(ex.getCause().getMessage())
+		.details(ex.getMessage())
+		.developerMessage(ex.getClass().getName())
+		.build();
+		
+		return new ResponseEntity<>(exceptionDetails, headers, status);
 	}
 	
 }
