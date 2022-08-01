8963cce5fcb599ad7c189235596850640572ccdd
guilhermewt <welliston10@gmail.com>
2022-08-01 13:41:15 -0300

    criando os test de jpa do (department)

---
+++ src/test/java/com/projetoTrabalhador/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.projetoTrabalhador.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.projetoTrabalhador.entities.Department;
+
+@DataJpaTest
+@DisplayName("test for department repository")
+public class DepartmentRepositoryTest {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+	
+	@Test
+	@DisplayName("save creater Department when successful")
+	void test(){
+		Department departmentToBeSaved = createDepartment();
+		Department departmentSaved = this.departmentRepository.save(departmentToBeSaved);
+		Assertions.assertThat(departmentSaved).isNotNull();
+		Assertions.assertThat(departmentSaved.getId()).isNotNull();
+		Assertions.assertThat(departmentSaved.getName()).isEqualTo(departmentToBeSaved.getName());
+	}
+	
+	private Department createDepartment() {
+		return Department.builder().name("informatica").build();
+	}
+
+}
